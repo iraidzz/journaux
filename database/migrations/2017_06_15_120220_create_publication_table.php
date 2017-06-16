bef0a72e386fa48933326cffa79a8f1c15857433
nelfire <girvincent@gmail.com>
2017-06-16 11:16:20 +0200

    Ajout date parution dans table publications

--- database/migrations/2017_06_15_120220_create_publication_table.php
+++ database/migrations/2017_06_15_120220_create_publication_table.php
@@ -20,6 +20,7 @@ class CreatePublicationTable extends Migration
             $table->string('photo_couverture');
             $table->string('description');
             $table->string('prix_annuel');
+            $table->dateTime('date_parution');
             $table->timestamps();
         });
     }
