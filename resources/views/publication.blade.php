023d3c64cd98b24d7356cddd61c8896c0c993e1d
nelfire <girvincent@gmail.com>
2017-06-16 10:48:33 +0200

    Formulaire Creer Publications

--- resources/views/publication.blade.php
+++ resources/views/publication.blade.php
@@ -3,14 +3,47 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">PUBLICATIONS MAGAZINES</div>
-
-                    <div class="panel-body">
-                        blablabla
-                    </div>
+            <div class="box text-center">
+                <div class="col-lg-12 text-center">
+                    <hr>
+                    <h2 class="intro-text text-center">Créer une
+                        <strong>publication</strong>
+                    </h2>
+                    <hr>
                 </div>
+                <div class="col-md-offset-3 col-md-6 text-center">
+                    <form role="form" action="" method="post">
+                        {!! csrf_field() !!}
+                        <div class="row">
+                            <input name="user_id" type="hidden" value="" class="form-control">
+                            <div class="form-group col-lg-6">
+                                <label>Titre publication</label>
+                                <input name="titre" type="text" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label>Image de l'article (URL Internet)</label>
+                                <input name="image" type="text" class="form-control">
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="form-group col-lg-12">
+                                <label>Texte publication</label>
+                                <textarea name="message" class="form-control" rows="6"></textarea>
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label>Prix à l'année</label>
+                                <input name="titre" type="text" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label>Nombre de parutions à l'année</label>
+                                <input name="titre" type="text" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <center><button type="submit" class="btn btn-default">Créer la publication.</button></center>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="clearfix"></div>
             </div>
         </div>
     </div>
