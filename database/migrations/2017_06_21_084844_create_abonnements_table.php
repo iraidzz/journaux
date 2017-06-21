13a84157b3d7f6901d5f363d08046f38ea89f318
iraidzz <iraidzz.13@gmail.com>
2017-06-21 11:24:16 +0200

    Suppression et création de table (ajout de s a chaque table + création propre à l'aide de la bonne commande) et modif des controleur pour adapte avec les nouvelles table + affichage historique avec les relations entre les tables fonctionnelles

---
+++ database/migrations/2017_06_21_084844_create_abonnements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAbonnementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abonnements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')/* ->unsigned() */;
+            /* $table->foreign('id_client')->references('id')->on('client'); */
+            $table->dateTime('date_debut');
+            $table->dateTime('date_fin');
+            $table->dateTime('date_pause');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abonnements');
+    }
+}
---
+++ database/migrations/2017_06_21_084856_create_historiques_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistoriquesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historiques', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id');
+            $table->integer('employe_id');
+            /* $table->foreign('id_employe')->references('id')->on('employe');*/
+            /* $table->integer('id_communication')->unsigned();
+             $table->foreign('id_communication')->references('id')->on('communication');*/
+            $table->string('type_communication');
+            $table->dateTime('date');
+            $table->longText('commentaire');
+
+        });
+
+        DB::table('historiques')->insert(
+            array(
+                'client_id' => 1,
+                'employe_id' => 2,
+                'type_communication' => 'téléphone',
+                'date' => '2016-05-12',
+                'commentaire' => 'client relou',
+
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historiques');
+    }
+}
