9c4b77eb166a585aacbacd22568381dccfe83a39
nelfire <girvincent@gmail.com>
2017-06-27 15:33:24 +0200

    Mise à niveau avec loick , on va travailler tous les deux sur API POST (pour préparer la connexion)

--- app/Http/Controllers/APIClientController.php
+++ app/Http/Controllers/APIClientController.php
@@ -24,62 +24,78 @@ class APIClientController extends Controller
 
     public function authentifier(Request $request)
     {
-        $publications = DB::table('publications')->get();
-        return response()->json(array(
-            'error' => false,
-            'result' => $publications,
-            'status_code' => 200
-        ));
-
+        //XXXXXX
     }
 
     public function enregistrer(Request $request)
     {
-        $post = $request->all();
-        $v = \Validator::make($request->all(),
-            [
-                'name' => 'required',
-                'email' => 'required',
-                'password' => 'required',
-                'prenom' => 'required',
-                'civilite' => 'required',
-                'numero_telephone' => 'required',
-                'date_naissance' => 'required',
-                'lieu_naissance' => 'required',
-                'adresse_domicile' => 'required',
-                'postal_domicile' => 'required',
-                'ville_domicile' => 'required',
-            ]);
-        if ($v->fails())
-        {
-            return redirect()->back()->withErrors($v->errors());
-        }
-        else
-        {
 
-            $ch = curl_init();
+        $ch = curl_init();
 
-            $data = [];
-            $data['user']['name'] = $request->only('name')['name'];
-            $data['user']['email'] = $request->only('email')['email'];
-            $data['user']['password'] = $request->only('password')['password'];
-            $data['user']['prenom'] = $request->only('prenom')['prenom'];
-            $data['user']['civilite'] = $request->only('civilite')['civilite'];
-            $data['user']['numero_telephone'] = $request->only('numero_telephone')['numero_telephone'];
-            $data['user']['date_naissance'] = $request->only('date_naissance')['date_naissance'];
-            $data['user']['lieu_naissance'] = $request->only('lieu_naissance')['lieu_naissance'];
-            $data['user']['adresse_domicile'] = $request->only('adresse_domicile')['adresse_domicile'];
-            $data['user']['postal_domicile'] = $request->only('postal_domicile')['postal_domicile'];
-            $data['user']['ville_domicile'] = $request->only('ville_domicile')['ville_domicile'];
+        $data = [];
+        $data['user']['name'] = $request->only('name')['name'];
+        $data['user']['email'] = $request->only('email')['email'];
+        $data['user']['password'] = $request->only('password')['password'];
+        $data['user']['prenom'] = $request->only('prenom')['prenom'];
+        $data['user']['civilite'] = $request->only('civilite')['civilite'];
+        $data['user']['numero_telephone'] = $request->only('numero_telephone')['numero_telephone'];
+        $data['user']['date_naissance'] = $request->only('date_naissance')['date_naissance'];
+        $data['user']['lieu_naissance'] = $request->only('lieu_naissance')['lieu_naissance'];
+        $data['user']['adresse_domicile'] = $request->only('adresse_domicile')['adresse_domicile'];
+        $data['user']['postal_domicile'] = $request->only('postal_domicile')['postal_domicile'];
+        $data['user']['ville_domicile'] = $request->only('ville_domicile')['ville_domicile'];
 
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 
-            // exécution de la session : $content est le retour de l'api
-            $content = json_decode(curl_exec($ch), true);
-            // fermeture de la session
-            curl_close($ch);
+        // exécution de la session : $content est le retour de l'api
+        $content = json_decode(curl_exec($ch), true);
+        // fermeture de la session
+        curl_close($ch);
 
 
-        }
+
+//        $post = $request->all();
+//        $v = \Validator::make($request->all(),
+//            [
+//                'name' => 'required',
+//                'email' => 'required',
+//                'password' => 'required',
+//                'prenom' => 'required',
+//                'civilite' => 'required',
+//                'numero_telephone' => 'required',
+//                'date_naissance' => 'required',
+//                'lieu_naissance' => 'required',
+//                'adresse_domicile' => 'required',
+//                'postal_domicile' => 'required',
+//                'ville_domicile' => 'required',
+//            ]);
+//        if ($v->fails())
+//        {
+//            return redirect()->back()->withErrors($v->errors());
+//        }
+//        else
+//        {
+//            $ch = curl_init();
+//
+//            $data = [];
+//            $data['user']['name'] = $request->only('name')['name'];
+//            $data['user']['email'] = $request->only('email')['email'];
+//            $data['user']['password'] = $request->only('password')['password'];
+//            $data['user']['prenom'] = $request->only('prenom')['prenom'];
+//            $data['user']['civilite'] = $request->only('civilite')['civilite'];
+//            $data['user']['numero_telephone'] = $request->only('numero_telephone')['numero_telephone'];
+//            $data['user']['date_naissance'] = $request->only('date_naissance')['date_naissance'];
+//            $data['user']['lieu_naissance'] = $request->only('lieu_naissance')['lieu_naissance'];
+//            $data['user']['adresse_domicile'] = $request->only('adresse_domicile')['adresse_domicile'];
+//            $data['user']['postal_domicile'] = $request->only('postal_domicile')['postal_domicile'];
+//            $data['user']['ville_domicile'] = $request->only('ville_domicile')['ville_domicile'];
+//
+//            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+//
+//            // exécution de la session : $content est le retour de l'api
+//            $content = json_decode(curl_exec($ch), true);
+//            // fermeture de la session
+//            curl_close($ch);
+//        }
     }
 }
